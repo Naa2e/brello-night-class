68026bb88885afa20cf5449fdccf3cce99fecb5f
Nicole <Nicole.Ahima@gmail.com>
2015-11-14 10:46:50 -0600

    Views

---
+++ Brello.Tests/Controllers/BoardControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Brello.Tests.Controllers
+{
+    [TestClass]
+    public class BoardControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
